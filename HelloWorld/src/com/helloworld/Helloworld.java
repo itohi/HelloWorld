34976f15886330bf95005b086b03992554eafc57
itohi <itoh@joy-tec.com>
2012-05-22 13:54:07 +0900

    日本語メソッド

--- HelloWorld/src/com/helloworld/Helloworld.java
+++ HelloWorld/src/com/helloworld/Helloworld.java
@@ -1,8 +1,28 @@
 package com.helloworld;
 
-public class Helloworld {
+/*public class Helloworld {
 
 	public static void main(String[] args){
 		System.out.println("HelloWorld");
-	}
-}
+	}*/
+
+	public class Helloworld {
+		  public static void main(String[] args) {
+		    int スコア = 40;   //(1)
+		    char グレード;
+
+		    if (スコア >= 90) {   //(2)
+		      グレード='A';
+		    } else if (スコア >= 70) {   //(3)
+		      グレード='B';
+		    } else if (スコア >= 50) {
+		      グレード='C';
+		    } else {
+		      グレード='D';
+		    }
+
+		    System.out.println("あなたは" + グレード + "判定です。");  //(4)
+		  }
+		}
+
+
